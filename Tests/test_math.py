26ac8ac0e02726e84d0dcef7891822a6192738ac
melissa <melissafisher.office@gmail.com>
2026-06-18 16:46:05 +0100

    An example of a failed test and how you review the console to see the results.

--- Tests/test_math.py
+++ Tests/test_math.py
@@ -1,9 +1,17 @@
-def test_one_plus_one():
-    assert 1 + 1 == 2
-
 """
 In the Terminal use the command python3 -m pytest to run the tests
 Print a dot for each passing test case
 Conclusion with a test summary.
 
 """
+def test_one_plus_one():
+    assert 1 + 1 == 2
+
+
+def test_one_plus_two():
+    a =1
+    b = 2
+    c = 0
+    assert a + b == c
+
+# Topic went over the details in the console you see such as failed tests.
