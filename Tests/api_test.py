44bfb276c3a9c8d54cde647f8c73010c373b2d93
melissa <melissafisher.office@gmail.com>
2026-06-22 20:59:00 +0100

    Practiced api testing.

--- Tests/api_test.py
+++ Tests/api_test.py
@@ -8,3 +8,16 @@ def test_get_booking_ids():
     assert isinstance(response.json(), list)
     print(f"Found {len(response.json())} bookings")
     return response.json()
+
+def test_health_check():
+    response = requests.get(f"{BASE_URL}/ping")
+    assert response.status_code == 201
+    assert response.text == "Created"
+
+# Test identified that the status code is incorrect - 201 instead of 200
+
+def test_auth_token():
+    response = requests.post(f"{BASE_URL}/auth", json={"username": "admin", "password": "password123"})
+    assert response.status_code == 200
+    return response.json()
+# changed the status code to 400 and fails as expected
